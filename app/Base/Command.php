ac47fdb66c3d37c933e5e2e6f37c15e333da5370
zhouyang <zhouyang@shein.com>
2019-08-15 17:49:05 +0800

    测试fork

--- app/Base/Command.php
+++ app/Base/Command.php
@@ -2,6 +2,7 @@
 
 namespace App\Base;
 
+use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
