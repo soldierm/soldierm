b8ce7ab84708dd1d7bea8e76ceac305846ef085f
zhouyang <zhouyang@shein.com>
2019-08-22 15:53:17 +0800

    测试swoole

---
+++ app/Console/Command/HttpServerCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Command;
+
+use App\Base\Command;
+use App\Console\Traits\Swoole\Server;
+use App\Http\App;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HttpServerCommand extends Command
+{
+    use Server;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this->setName('httpserver')
+            ->setDescription('Manage swoole tcp server.')
+            ->setHelp("This command allows you to manage swoole http server...");
+
+        $this->addOption('start', 's', InputOption::VALUE_OPTIONAL, 'start http server');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $server = $this->getHttpServer(['host' => '119.23.200.233']);
+        $server->set([
+            'worker_num' => 4,
+            'backlog' => 128,
+            'max_request' => 5,
+            'dispatch_mode' => 1,
+            'reactor_num' => 2,
+        ]);
+        $httpApp = new App(CONFIG_PATH);
+        $server->on('request', function(Request $request, Response $response) use ($httpApp) {
+//            $response->end("<h1>hello swoole</h1>");
+            $httpApp->run();
+        });
+        $server->start();
+    }
+}
